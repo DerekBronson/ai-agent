c3eccabbb5b8e792a0be7f9a137cf6e1f3b39bc7
Derek Bronson <derekbronson@Dereks-MacBook-Pro.local>
2025-07-17 06:52:20 -0700

    Passed the first couple of lessons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,14 +7,19 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
+    if len(sys.argv) == 1:
+        print ("Error: No prompt provided")
+        sys.exit(1)
+
     client = genai.Client(api_key=api_key)
-    test_content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    test = client.models.generate_content(model='gemini-2.0-flash-001', contents=test_content)
+    prompt = sys.argv[1]
+    #test_content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    ai_response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
     
-    print (f"Text returned is {test.text}")
-    metadata = test.usage_metadata
-    print (f"Prompt tokens: {metadata["prompt_token_count"]}")
-    print (f"Response tokens: {metadata["candidates_token_count"]}")
+    print (f"Text returned is {ai_response.text}")
+    #print (f"Metadata returned is {test.usage_metadata}")
+    print (f"Prompt tokens: {ai_response.usage_metadata.prompt_token_count}")
+    print (f"Response tokens: {ai_response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
